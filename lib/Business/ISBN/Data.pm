25768ec8b7ff237b11a58f27a6b4b23fab3aa358
brian d foy <brian.d.foy@gmail.com>
2021-02-15 12:42:56 -0500

    Bump to next dev version

--- lib/Business/ISBN/Data.pm
+++ lib/Business/ISBN/Data.pm
@@ -7,7 +7,7 @@ use Carp                  qw(carp);
 use File::Basename        qw(dirname);
 use File::Spec::Functions qw(catfile);
 
-our $VERSION = '20210112.005_02';
+our $VERSION = '20210112.005_03';
 
 =encoding utf8
 
